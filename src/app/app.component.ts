8ab14c66e7a2f2f89047cd7501be14f3f0fcc019
aredelso <aredelso@syr.edu>
2017-01-30 16:15:05 -0800

    binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,18 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.css'],
 })
 export class AppComponent {
   title = 'app works!, woop woop.';
   name = 'ninja';
+  ninja = {
+    name: "Ryu",
+    belt: "Red"
+  };
+
+  yell(e) {
+    alert("I am yelling");
+  }
 }
